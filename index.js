e9758278f874abbc107686664cd8c7e9c6859690
thestraded <thestraded@gmail.com>
2018-01-31 14:08:57 -0500

    can loop through and set size param

--- index.js
+++ index.js
@@ -1,16 +1,112 @@
-$(function() {
-// questionable name
-var childHolder = [];
-  $.fn.superSlider= function(){
-    // actually grab the items and add them to an array then make them into the slideshow
-    for (var i = 0; i < $( "#slider" ).find("img").length; i++) {
-      childHolder.push($( "#slider" ).find("img")[i].currentSrc)
-    }
 
-    this.css("color", "red")
+
+
+
+$.fn.superSlider= function(opts){
+  const options = {
+    display: opts.display || 5,
+    listContainer: opts.listContainer || "ul",
   }
-  console.log(childHolder)
 
 
-  $("#slider").superSlider()
+  $(this).each(function() {
+    var totalElements = $(this).children("ul").children("li").length
+    var currentSet = 0;
+    var len = $(this).children(options.listContainer).length;
+    var that = this;
+    var displayAmount = options.display;
+    onLoad()
+
+    $(this).css("border", "1px solid red");
+
+    $(this).children("button:first").click(function() {
+      rotateSet(false);
+    })
+
+    $(this).children("button:last").click(function() {
+      rotateSet(true);
+    })
+
+
+    function rotateSet(isNext) {
+      // adding the display amount will get you the new starting position
+      // $(that).children("ul").children("li").css("background-color","red")
+      $(that).children("ul").children("li").show()
+
+      if (isNext) {
+        currentSet = currentSet + displayAmount
+        if (currentSet >= totalElements) {
+          currentSet = currentSet-totalElements
+        }
+      }else {
+        currentSet = currentSet - displayAmount
+        if (currentSet < 0) {
+          currentSet = totalElements+currentSet
+        }
+      }
+      holder = currentSet+ displayAmount-1;
+      console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      console.log("holder", holder);
+      console.log("current pos ", currentSet);
+      console.log("Amount shown ", displayAmount);
+      console.log("Total elements ", totalElements);
+
+      // if currentSet + displayAmount is larger than the total elements there will be wrapping
+      if (currentSet + displayAmount > totalElements) {
+        console.log("WRAPPING SHOULD OCCUR");
+          // if there is overflow and the current pos isnt exactly overflowing
+          console.log("Warpping but currentPos doesnt");
+          remainder = currentSet + displayAmount - totalElements -1;
+          $(that).children("ul").children("li:lt("+ currentSet + "):gt("+ remainder + ")").hide()
+          console.log(remainder);
+      } else {
+        console.log("NORMAL CASE NO WRAPPING");
+        $(that).children("ul").children("li:lt("+ currentSet + ")").hide()
+        $(that).children("ul").children("li:gt("+ holder + ")").hide()
+      }
+      }
+
+    function onLoad(displayAmount){
+      displayFix = options.display-1
+      $(that).children("ul").children("li:gt("+ displayFix + ")").hide()
+    }
+    //on-load
+      //display only required amount
+
+  })
+}
+
+$(".slider").superSlider({
+  display:4,
+
 });
+
+
+
+
+
+// var childHolder = [];
+// var currentIterator = 0;
+//
+// $(function() {
+// // questionable name
+//
+//
+//   $.fn.superSlider= function(){
+//     // actually grab the items and add them to an array then make them into the slideshow
+//     for (var i = 0; i < $( "#slider" ).find("img").length; i++) {
+//       childHolder.push($( "#slider" ).find("img")[i].currentSrc)
+//     }
+//     $( "#slider" ).find(".column").remove()
+//     this.css("background-color", "red")
+//   }
+//   $("#slider").superSlider()
+//   console.log(childHolder[currentIterator]);
+//
+//
+//   // $.fn.sliderSelect= function(){
+//       $( "#slider" ).append( "<div class='column'> <img src = '" + childHolder[0] + "' ></div>" );
+//
+//   // }
+//
+// });
