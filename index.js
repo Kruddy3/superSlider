74b287d467b2188b6932ba54b47e9e7d0f17a0d9
thestraded <thestraded@gmail.com>
2018-01-30 21:35:36 -0500

    selecting the imgs within the div given and putting in an array

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 $(function() {
+// questionable name
+var childHolder = [];
   $.fn.superSlider= function(){
+    // actually grab the items and add them to an array then make them into the slideshow
+    for (var i = 0; i < $( "#slider" ).find("img").length; i++) {
+      childHolder.push($( "#slider" ).find("img")[i].currentSrc)
+    }
+
     this.css("color", "red")
   }
+  console.log(childHolder)
 
 
   $("#slider").superSlider()
